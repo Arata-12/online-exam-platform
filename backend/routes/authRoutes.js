7cf0c9a8fbccb1dc55b9f1dce802a26c5336d95a
Arata-12 <bar164941@gmail.com>
2025-04-25 15:02:08 +0100

    make route for login and register

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,1 +1,14 @@
 // Auth routes
+import express from 'express';
+import { RegisterUser, LoginUser } from '../controllers/authController.js';
+
+const router = express.Router();
+
+// POST /api/auth/register
+router.post('/register', RegisterUser);
+
+// POST /api/auth/login
+router.post('/login', LoginUser);
+
+
+export default router;
