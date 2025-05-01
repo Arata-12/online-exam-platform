2cc95624687227e5ae684f9118a3203623d55a53
Arata-12 <bar164941@gmail.com>
2025-05-02 00:05:23 +0100

    update Exam, Question, and Result schemas

--- backend/models/Result.js
+++ backend/models/Result.js
@@ -1,1 +1,31 @@
 // Result model queries
+import db from '../config/db.js';
+
+// Insert a new result after exam submission
+export const insertResult = (student_exam_id, total_score, total_time_seconds, status, submission_time) => {
+  return new Promise((resolve, reject) => {
+    const sql = `
+      INSERT INTO \`results\`
+      (\`student_exam_id\`, \`total_score\`, \`total_time_seconds\`, \`status\`, \`submission_time\`)
+      VALUES (?, ?, ?, ?, ?)
+    `;
+    db.query(sql, [student_exam_id, total_score, total_time_seconds, status, submission_time], (err, result) => {
+      if (err) return reject(err);
+      resolve(result.insertId);
+    });
+  });
+};
+
+// Get final result of a student_exam
+export const getResultByStudentExamId = (student_exam_id) => {
+    return new Promise((resolve, reject) => {
+      const sql = `
+        SELECT * FROM \`results\`
+        WHERE \`student_exam_id\` = ?
+      `;
+      db.query(sql, [student_exam_id], (err, result) => {
+        if (err) return reject(err);
+        resolve(result[0]);
+      });
+    });
+  };
